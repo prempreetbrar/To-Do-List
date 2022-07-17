e7f95bb685aef32e52acf2a42da158066f909cba
prempreetbrar <prempreet.brar@ucalgary.ca>
2022-07-17 13:37:41 -0600

    [list-7] Refactored NewToDoForm component

--- src/NewToDoForm.js
+++ src/NewToDoForm.js
@@ -3,12 +3,14 @@ import "./NewToDoForm.css"
 import { useState } from "react";
 
 import AddIcon from '@mui/icons-material/Add';
-import { IconButton } from "@mui/material";
-
+import { Tooltip, IconButton} from "@mui/material";
 import {v4 as uuidv4} from "uuid";
+
+
 
 export default function NewToDoForm({addToList}) {
   const [task, setTask] = useState("");
+
 
   function handleInput(event) {
     setTask(event.target.value);
@@ -17,19 +19,30 @@ export default function NewToDoForm({addToList}) {
   function handleSubmit(event) {
     event.preventDefault();
     addToList({id: uuidv4(), task, isCompleted: false});
+    // empty the new task field after we've added our task to the list
     setTask("");
+  }
+
+
+  function AddButton() {
+    return (
+      <IconButton 
+        sx={{marginLeft: "0.5rem", marginBottom: "3rem"}} 
+        onClick={handleSubmit}
+      >
+        <AddIcon fontSize="large" className="NewToDoForm-addIcon"/>
+      </IconButton>
+    );
   }
 
   return (
     <div className="NewToDoForm">
-      <span>Add to the ToDo List
-      </span>
+      <span>Add to the ToDo List</span>
+
       <form>
         <textarea className="NewToDoForm-taskInput" name="task" placeholder={"New Task"} onInput={handleInput} value={task}/>
-        <IconButton sx={{marginLeft: "0.5rem", marginBottom: "3rem"}}onClick={handleSubmit}>
-          <AddIcon fontSize="large" sx={{color: "white"}}/>
-        </IconButton>
+        {AddButton()}
       </form>
     </div>
-  )
+  );
 }
